e79c98f87b97fd7c35706f9bc6665fd26881c89a
Harun <harunabdi8@gmail.com>
2024-06-26 15:03:39 +0200

    we

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,5 +5,5 @@ const app = express();
 const port: number = 3000;
 
 app.listen(port, () => {
-  console.log("Server is running on port ", port);
+  console.log("Server is running on portttt ", port);
 });
