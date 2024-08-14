596af39182044e9054747f83bebadd63e5ca662a
Harun <harunabdi8@gmail.com>
2024-08-14 17:12:26 +0200

    yoyo

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,15 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist",
+    chunkSizeWarningLimit: 1000, // Increase the chunk size limit to 1000 kB
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: "/src/main.jsx",
+    },
+    // other build options
+  },
   plugins: [react()],
 });
